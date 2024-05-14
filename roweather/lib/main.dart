3b4cb84082ef8675cf1dd93d5e356298e3bcb353
Robert Dancer <robertdancer@ymail.com>
2024-05-14 13:36:32 +0100

    Moved settings to a new file

--- roweather/lib/main.dart
+++ roweather/lib/main.dart
@@ -1,4 +1,5 @@
 import 'sections/calendar.dart';
+import 'sections/settings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -96,8 +97,10 @@ class _MyHomePageState extends State<MyHomePage> {
             ListTile(
               title: const Text('Book an Outing'),
               onTap: () {
-                // Update the state of the app.
-                // ...
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeCalendar()),
+                );
               },
             ),
             ListTile(
@@ -106,15 +109,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const SettingsPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Calendar'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeCalendar()),
                 );
               },
             ),
@@ -157,42 +151,6 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         )
         ]
-      ),
-    );
-  }
-}
-
-
-class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-  @override
-  State<SettingsPage> createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Back'),
-        ),
       ),
     );
   }
