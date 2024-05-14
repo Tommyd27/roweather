9e856cca47d922392d9588edb09501b8b5262034
TOM BRENNAN <tom@smart-homes.com>
2024-05-14 15:42:28 +0100

    Added texts and checkboxes to settings

--- roweather/lib/pages/settings.dart
+++ roweather/lib/pages/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'calendar.dart';
 import 'sections/sidebar.dart';
+
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
@@ -18,23 +19,46 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   var scaffoldKey = GlobalKey<ScaffoldState>();
-
+  bool _isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       drawer: Sidebar(),
       body: Stack(children: <Widget>[
+        Container(
+          color: Colors.green,
+          width: double.infinity,
+        ),
         Positioned(
-          left: 10,
-          top: 20,
-          child: IconButton(
-            icon: Icon(Icons.menu, color: Colors.white),
-            onPressed: () => scaffoldKey.currentState!.openDrawer(),
-          )
+            left: 10,
+            top: 20,
+            child: IconButton(
+              icon: Icon(Icons.menu, color: Colors.white),
+              onPressed: () => scaffoldKey.currentState!.openDrawer(),
+            )),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              child: Text('Settings', style: TextStyle(fontSize: 24)),
+            ),
+            Center(
+              child: Text('Notifications', style: TextStyle(fontSize: 24)),
+            ),
+            CheckboxListTile(
+              title: Text("Label"),
+              value: _isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  _isChecked = value!;
+                });
+              },
+              controlAffinity: ListTileControlAffinity.leading,
+            )
+          ],
         )
-        ]
-      ),
+      ]),
     );
   }
 }
