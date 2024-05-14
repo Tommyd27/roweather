5b78dcc874b2ce248735d0963ec2e0e7fa93d6f0
TOM BRENNAN <tom@smart-homes.com>
2024-05-14 14:40:06 +0100

    Some changes will immedatiely be undone when merging

--- roweather/lib/main.dart
+++ roweather/lib/main.dart
@@ -1,5 +1,6 @@
 import 'pages/sections/calendar.dart';
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(BaseApp());
@@ -8,7 +9,7 @@ void main() {
 class BaseApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
+      home: HomePage(),
     );
 
   }
---
+++ roweather/lib/pages/settings.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'sections/calendar.dart';
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+ var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      key: scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            SizedBox(
+              height: 100,
+              child: DrawerHeader(
+                padding: EdgeInsets.only(left: 10, top: 0),
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+                  child: Align(
+                    alignment: Alignment(-1, -1),
+                    child: IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: () => scaffoldKey.currentState!.closeDrawer(),
+                    )
+                  )
+                
+              )
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Book an Outing'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeCalendar()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Stack(children: <Widget>[
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/sculling-cropped.png"),
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            height: 600,
+          ),
+          Container(
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [
+                    Color(0x00003330),
+                    Color(0xFF003330),
+                  ],
+                  stops: [
+                    0.3,
+                    0.6,
+                  ])),
+        ),
+        Positioned(
+          left: 10,
+          top: 20,
+          child: IconButton(
+            icon: Icon(Icons.menu, color: Colors.white),
+            onPressed: () => scaffoldKey.currentState!.openDrawer(),
+          )
+        )
+        ]
+      ),
+    );
+  }
+}
