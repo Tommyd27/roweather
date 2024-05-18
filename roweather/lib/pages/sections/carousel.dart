26e4fd864a8ee9b018e5a370b0f09cb90713f399
radhika735 <radhe.k.agrawal@gmail.com>
2024-05-18 03:31:24 +0100

    new version of carousel

--- roweather/lib/pages/sections/carousel.dart
+++ roweather/lib/pages/sections/carousel.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/* import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:carousel_slider/carousel_options.dart';
 
@@ -48,6 +48,37 @@ class _CarouselState extends State<Carousel> {
           },
         );
       }).toList(),
+    );
+  }
+} */
+
+import 'package:flutter/material.dart';
+
+class NextDaysCarousel extends StatefulWidget {
+  const NextDaysCarousel({super.key});
+
+  @override
+  State<NextDaysCarousel> createState() => _NextDaysCarouselState();
+}
+
+class _NextDaysCarouselState extends State<NextDaysCarousel> {
+  List<String> carouselDays= ['Today', 'Tomorrow'];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemExtent: 20.0,
+      padding: const EdgeInsets.all(8),
+      scrollDirection: Axis.horizontal,
+      itemCount: carouselDays.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(carouselDays[index]),
+          onTap: (){},
+          tileColor: Color(0xff85B09A)
+        );
+      } ,
+      
     );
   }
 }
