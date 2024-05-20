7c25452bd05a3b32b3689de2b35bbede7f8c254f
AxiomNine <ds2089@cam.ac.uk>
2024-05-20 23:34:05 +0100

    Comments complete

--- roweather/lib/pages/calendar.dart
+++ roweather/lib/pages/calendar.dart
@@ -1,4 +1,4 @@
-//import 'dart:ffi';
+//All the packages we are using for this page
 
 import 'package:demo/pages/appstate.dart';
 import 'package:demo/pages/homepage.dart';
@@ -23,13 +23,13 @@ class _BookingPageState extends State<BookingPage> {
   DateTime _focusedCalendarDate = DateTime.now();
   DateTime? selectedCalendarDate;
 
-  int _startHour = 0;
+  int _startHour = 0; //The variables representing start and end times
   int _startMin = 0;
   int _endHour = 0;
   int _endMin = 0;
-  String _textPrompt = '';
+  String _textPrompt = ''; //This changes the text prompt we may need to display to notify users that they have made an error
 
-  late FixedExtentScrollController _controller1;
+  late FixedExtentScrollController _controller1; //These make it possible to choose a booking time
   late FixedExtentScrollController _controller2;
   late FixedExtentScrollController _controller3;
   late FixedExtentScrollController _controller4;
@@ -51,7 +51,7 @@ class _BookingPageState extends State<BookingPage> {
         appBar: AppBar(
           leading: GestureDetector(
             child: const Icon(
-              Icons.close, // add custom icons also
+              Icons.close, // The exit button, to bring the user back to the Home Page
               color: Colors.white,
             ),
             onTap: () => {
@@ -59,25 +59,10 @@ class _BookingPageState extends State<BookingPage> {
             },
           ),
           backgroundColor: Color(0xFF4e7c65),
-          /*title: const Row(
-            children: [
-              Center(
-                child: Text(
-                  'Select date and time to schedule:',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),*/
           title: const FittedBox(
             alignment: Alignment.topLeft,
             fit: BoxFit.fitWidth,
-            child: Text(
+            child: Text( //The title, which can shrink to fit the device's width
               'Select date and time to schedule:',
               style: TextStyle(
                 fontFamily: 'Roboto',
@@ -92,12 +77,12 @@ class _BookingPageState extends State<BookingPage> {
           alignment: Alignment.center,
           width: MediaQuery.of(context).size.width,
           color: Color(0xFF4e7c65),
-          child: SingleChildScrollView(
+          child: SingleChildScrollView( //This is to make "scrolling down" possible
             child: Column(
               children: [
                 SingleChildScrollView(
                     child: Column(children: <Widget>[
-                  const Card(
+                  const Card( //This acts as vertical padding
                       margin: EdgeInsets.all(8.0),
                       elevation: 5.0,
                       shape: RoundedRectangleBorder(
@@ -108,16 +93,16 @@ class _BookingPageState extends State<BookingPage> {
                               color: Color(0xff85B09A), width: 2.0))),
                   Container(
                     color: Colors.white,
-                    child: TableCalendar(
+                    child: TableCalendar( //An off-the-shelf calendar-based widget, with customisations
                       focusedDay: _focusedCalendarDate,
                       firstDay: DateTime.now(),
-                      lastDay: DateTime.now().add(const Duration(days: 60)),
+                      lastDay: DateTime.now().add(const Duration(days: 60)), //Ensures a two-month advance booking limit
                       calendarFormat: _calendarFormat,
                       weekendDays: const [DateTime.sunday, 6],
                       startingDayOfWeek: StartingDayOfWeek.monday,
                       daysOfWeekHeight: 40.0,
                       rowHeight: 60.0,
-                      headerStyle: const HeaderStyle(
+                      headerStyle: const HeaderStyle( //Customises the calendar header
                         titleTextStyle:
                             TextStyle(color: Colors.white, fontSize: 20.0),
                         decoration: BoxDecoration(
@@ -156,11 +141,11 @@ class _BookingPageState extends State<BookingPage> {
                             color: Color(0xff808080),
                             shape: BoxShape.circle,
                           )),
-                      selectedDayPredicate: (currentSelectedDate) {
+                      selectedDayPredicate: (currentSelectedDate) { //Marks the current date
                         return (isSameDay(
                             selectedCalendarDate, currentSelectedDate));
                       },
-                      onDaySelected: (selectedDay, focusedDay) {
+                      onDaySelected: (selectedDay, focusedDay) { //Ensures that one can select days - and adapts the page state accordingly
                         if (!isSameDay(selectedCalendarDate, selectedDay)) {
                           setState(() {
                             selectedCalendarDate = selectedDay;
@@ -168,12 +153,12 @@ class _BookingPageState extends State<BookingPage> {
                           });
                         }
                       },
-                      onPageChanged: (focusedDay) {
+                      onPageChanged: (focusedDay) { //Ensures the user can select days on a particular month, and focuses there
                         _focusedCalendarDate = focusedDay;
                       },
                     ),
                   ),
-                  const Card(
+                  const Card( //More padding
                       margin: EdgeInsets.all(8.0),
                       elevation: 5.0,
                       shape: RoundedRectangleBorder(
@@ -184,9 +169,9 @@ class _BookingPageState extends State<BookingPage> {
                               color: Color(0xff85B09A), width: 2.0))),
                 ])),
                 SingleChildScrollView(
-                  child: SizedBox(
+                  child: SizedBox(//This contains the time selector wheels
                     height: 200,
-                    width: MediaQuery.of(context).size.width * 0.9,
+                    width: MediaQuery.of(context).size.width * 0.9, //Box width can adapt depending on screen size
                     child: Container(
                       decoration: BoxDecoration(
                         color: Color(0xffA6C5B5),
@@ -200,14 +185,14 @@ class _BookingPageState extends State<BookingPage> {
                               child: Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Container(
+                                  Container( //A label
                                       child: const Text('Start time:',
                                           style: TextStyle(
                                             fontFamily: 'Roboto',
                                             fontSize: 32,
                                           ))),
                                   Container(
-                                    decoration: BoxDecoration(
+                                    decoration: BoxDecoration(//The wheel's background
                                         color: Colors.white,
                                         borderRadius:
                                             BorderRadius.circular(20)),
@@ -218,12 +203,12 @@ class _BookingPageState extends State<BookingPage> {
                                         Container(
                                           child: SizedBox(
                                             height: 100,
-                                            width: MediaQuery.of(context)
+                                            width: MediaQuery.of(context) //Again, width dependent on screen size
                                                     .size
                                                     .width *
                                                 0.12,
                                             child:
-                                                ListWheelScrollView.useDelegate(
+                                                ListWheelScrollView.useDelegate( //A selector wheel for the hour of the start time
                                               controller: _controller1,
                                               itemExtent: 50,
                                               perspective: 0.005,
@@ -259,7 +244,7 @@ class _BookingPageState extends State<BookingPage> {
                                                     .width *
                                                 0.12,
                                             child:
-                                                ListWheelScrollView.useDelegate(
+                                                ListWheelScrollView.useDelegate( //A selector wheel for the minute of the start time
                                               controller: _controller2,
                                               itemExtent: 50,
                                               perspective: 0.005,
@@ -293,7 +278,7 @@ class _BookingPageState extends State<BookingPage> {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Container(
-                                  child: const Text('End time:',
+                                  child: const Text('End time:', //Another label
                                       style: TextStyle(
                                           fontFamily: 'Roboto', fontSize: 32))),
                               Container(
@@ -309,7 +294,7 @@ class _BookingPageState extends State<BookingPage> {
                                         width:
                                             MediaQuery.of(context).size.width *
                                                 0.12,
-                                        child: ListWheelScrollView.useDelegate(
+                                        child: ListWheelScrollView.useDelegate( //A selector wheel for the hour of the end time
                                           controller: _controller3,
                                           itemExtent: 50,
                                           perspective: 0.005,
@@ -342,7 +327,7 @@ class _BookingPageState extends State<BookingPage> {
                                         width:
                                             MediaQuery.of(context).size.width *
                                                 0.12,
-                                        child: ListWheelScrollView.useDelegate(
+                                        child: ListWheelScrollView.useDelegate( //A selector wheel for the minute of the end time
                                           controller: _controller4,
                                           itemExtent: 50,
                                           perspective: 0.005,
@@ -373,7 +358,7 @@ class _BookingPageState extends State<BookingPage> {
                     ),
                   ),
                 ),
-                const Card(
+                const Card( //Padding, again
                     margin: EdgeInsets.all(8.0),
                     elevation: 5.0,
                     shape: RoundedRectangleBorder(
@@ -383,14 +368,14 @@ class _BookingPageState extends State<BookingPage> {
                         side:
                             BorderSide(color: Color(0xff85B09A), width: 2.0))),
                 Container(
-                  decoration: BoxDecoration(
+                  decoration: BoxDecoration( //The "Book" button
                       color: Color(0xFFA6C5B5),
                       borderRadius: const BorderRadius.all(
                         Radius.circular(25.0),
                       ),
                       border: Border.all(color: Color(0xff639067), width: 4),
                       boxShadow: [
-                        BoxShadow(
+                        BoxShadow( //Included to make it look like a button
                             color: Color(0xFF444444),
                             spreadRadius: 3,
                             blurRadius: 5,
@@ -401,7 +386,7 @@ class _BookingPageState extends State<BookingPage> {
                   child: Center(
                     child: GestureDetector(
                       onTap: () {
-                        if (selectedCalendarDate == null) {
+                        if (selectedCalendarDate == null) { //Cases here to detect user error
                           setState(() {
                             _textPrompt = 'Please select a date';
                           });
@@ -410,7 +395,7 @@ class _BookingPageState extends State<BookingPage> {
                           setState(() {
                             _textPrompt = 'Outing cannot end before it starts';
                           });
-                        } else {
+                        } else { //Here, the data structures are built for the AppState
                           DateTime key = DateTime(
                               selectedCalendarDate!.year,
                               selectedCalendarDate!.month,
@@ -419,15 +404,15 @@ class _BookingPageState extends State<BookingPage> {
                               hour: _startHour + 5, minute: _startMin * 5);
                           TimeOfDay end = TimeOfDay(
                               hour: _endHour + 5, minute: _endMin * 5);
-                          if (Provider.of<AppState>(context, listen: false)
+                          if (Provider.of<AppState>(context, listen: false) //The AppState tries to add a booking
                               .addOuting(key, start, end)) {
-                            Navigator.push(
+                            Navigator.push( //The app returns to the home page if the booking is successful
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) =>
                                         MyHomePage(title: 'Roweather')));
                           } else {
-                            setState(() {
+                            setState(() { //The appState will not book if outings clash
                               _textPrompt =
                                   'Outing seems to clash with another';
                             });
@@ -444,7 +429,7 @@ class _BookingPageState extends State<BookingPage> {
                     ),
                   ),
                 ),
-                const Card(
+                const Card( //Vertical padding
                     margin: EdgeInsets.all(8.0),
                     elevation: 5.0,
                     shape: RoundedRectangleBorder(
@@ -453,7 +438,7 @@ class _BookingPageState extends State<BookingPage> {
                         ),
                         side:
                             BorderSide(color: Color(0xff85B09A), width: 2.0))),
-                Container(
+                Container( //This displays any prompts in case of user error
                   height: 100,
                   child: Text(
                     _textPrompt,
