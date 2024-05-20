b2a4f60bff2526eea7cd0e565946d1e6828a8330
Tom Brennan <tom@smart-homes.com>
2024-05-20 20:15:33 +0100

    fixed river level bug

--- roweather/lib/pages/sections/selectedDay.dart
+++ roweather/lib/pages/sections/selectedDay.dart
@@ -15,29 +15,26 @@ class TextComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-        children: [
-          Text(
-            mainText,
-            style: GoogleFonts.urbanist(
-                textStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 48,
-                    fontWeight: FontWeight.w700)),
-          ),
-          Container(
+      children: [
+        Text(
+          mainText,
+          style: GoogleFonts.urbanist(
+              textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w700)),
+        ),
+        Container(
             margin: const EdgeInsets.only(left: 5),
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-            Text(sideUpText ?? "",
-                style: const TextStyle(color: Colors.white, fontSize: 18)),
-            Text(sideDownText ?? "",
-                style: const TextStyle(color: Colors.white, fontSize: 18)),
-          ])
-          )
-          
-        ],
-      );
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(sideUpText ?? "",
+                  style: const TextStyle(color: Colors.white, fontSize: 18)),
+              Text(sideDownText ?? "",
+                  style: const TextStyle(color: Colors.white, fontSize: 18)),
+            ]))
+      ],
+    );
   }
 }
 
@@ -47,46 +44,54 @@ class SelectedDay extends StatelessWidget {
   var formatter = NumberFormat("#0.0");
 
   @override
-  Widget build(BuildContext context) => 
-    Consumer<AppState>(builder: (context, appstate, child) {
-      DailyWeather? info = appstate.daily[appstate.daySelectedIndex];
-      double? temp = info?.temperature;
-      double? wind = info?.windSpeed;
-      String tempSign = "°C";
-      String speedUnits = "KM/H";
-      String lengthUnits = "m";
-      if (appstate.settings.unitTemperature == "Fahrenheit") {
-        temp = 1.8 * temp! + 32;
-        tempSign = '°F';
-      }
-      if (appstate.settings.unitSpeed == "MPH") {
-        wind = wind! / 1.609;
-        speedUnits = "MPH";
-      }
-      if (appstate.settings.unitHeight == "Feet") {
-        riverLevel = riverLevel! * 3.281;
-        lengthUnits = "ft";
-      }
-      return Container(
-        margin: const EdgeInsets.all(8),
-        child: Column(children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                TextComponent('${NumberFormat("#0").format(temp ?? 19)}' + tempSign),
-                TextComponent(formatter.format(wind ?? 8.2), sideUpText: speedUnits, sideDownText: "Wind"),
-                TextComponent("2.3", sideUpText: speedUnits, sideDownText: "Water"), // TODO
-              ]),
-          Container(
-            margin: const EdgeInsets.only(top: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                TextComponent(info.uvIndex.toString(), sideUpText: "UV"),
-                appstate.riverLevel == null ? const CircularProgressIndicator() : TextComponent(formatter.format(appstate.estimateRiverLevel(info.day)), sideUpText: lengthUnits, sideDownText: "River Level") // TODO
-              ])
-            )
-          
-        ]));
-  }); 
+  Widget build(BuildContext context) =>
+      Consumer<AppState>(builder: (context, appstate, child) {
+        DailyWeather? info = appstate.daily[appstate.daySelectedIndex];
+        double? temp = info?.temperature;
+        double? wind = info?.windSpeed;
+        String tempSign = "°C";
+        String speedUnits = "KM/H";
+        String lengthUnits = "m";
+        if (appstate.settings.unitTemperature == "Fahrenheit") {
+          temp = 1.8 * temp! + 32;
+          tempSign = '°F';
+        }
+        if (appstate.settings.unitSpeed == "MPH") {
+          wind = wind! / 1.609;
+          speedUnits = "MPH";
+        }
+        if (appstate.settings.unitHeight == "Feet") {
+          appstate.riverLevel = appstate.riverLevel! * 3.281;
+          lengthUnits = "ft";
+        }
+        return Container(
+            margin: const EdgeInsets.all(8),
+            child: Column(children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    TextComponent(
+                        '${NumberFormat("#0").format(temp ?? 19)}' + tempSign),
+                    TextComponent(formatter.format(wind ?? 8.2),
+                        sideUpText: speedUnits, sideDownText: "Wind"),
+                    TextComponent("2.3",
+                        sideUpText: speedUnits, sideDownText: "Water"), // TODO
+                  ]),
+              Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        TextComponent(info.uvIndex.toString(),
+                            sideUpText: "UV"),
+                        appstate.riverLevel == null
+                            ? const CircularProgressIndicator()
+                            : TextComponent(
+                                formatter.format(
+                                    appstate.estimateRiverLevel(info.day)),
+                                sideUpText: lengthUnits,
+                                sideDownText: "River Level") // TODO
+                      ]))
+            ]));
+      });
 }
